c4a33ca592ff3882d6e09267abc75c9067fb2ca5
Serdar <serdarbaal@gmail.com>
2022-07-03 01:42:40 +0300

    commit

--- main.java
+++ main.java
@@ -1,3 +1,22 @@
-public class main {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class openBrowser {
+    public static void main(String[] args) {
+        Path resourceDirectory = Paths.get("src", "main", "resources");
+        String absolutePath = resourceDirectory.toFile().getAbsolutePath();
+
+        System.out.println(absolutePath);
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\srdrb\\IdeaProjects\\untitled3\\src\\main\\resources\\binary\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        String url = "https://demoqa.com/webtables/";
+        driver.get(url);
+        driver.findElement(By.xpath("//*[@autocomplete=\"off\"]")).click();
+    }
 }
